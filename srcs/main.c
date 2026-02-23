c4b4d1acf532f09618c4ab1867ef7910f8449720
DiogoLiebner <diogoliebner@gmail.com>
2026-02-23 15:40:29 +0000

    added main.c

--- srcs/main.c
+++ srcs/main.c
@@ -12,6 +12,21 @@
 
 #include "push_swap.h"
 
+void	sort_stack(t_stack *stack_a, t_stack *stack_b)
+{
+if (!is_sorted(stack_a))
+	{
+		if (stack_size(stack_a) == 2)
+			sa(&stack_a, 1);
+		else if (stack_size(stack_a) == 3)
+			sort_three(&stack_a);
+		else if (stack_size(stack_a) <= 5)
+			sort_small(&stack_a, &stack_b);
+		else
+			sort_large(&stack_a, &stack_b);
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	t_stack	*stack_a;
@@ -23,5 +38,14 @@ int	main(int argc, char **argv)
 	{
 		return (0);
 	}
-	
+	initstack(&stack_a, argc, argv);
+	if (!stack_a || has_duplicates(stack_a))
+	{
+		free_stack(&stack_a);
+		error_exit();
+	}
+	sort_stack(stack_a, stack_b);
+	free_stack(&stack_a);
+	free_stack(&stack_b);
+	return (0);
 }
