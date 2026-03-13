790591cddbe28b9f07ee752406b8493d189eaf82
dlima-li <38533011+DiogoLiebner@users.noreply.github.com>
2026-03-13 18:20:00 +0000

    Fix parsing logic for numeric string validation

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -20,7 +20,11 @@ int	is_valid_number(char *str)
 	if (!str[i])
 		return (0);
 	if (str[i] == '-' || str[i] == '+')
+	{
 		i++;
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+	}
 	while (str[i])
 	{
 		if (str[i] < '0' || str[i] > '9')
